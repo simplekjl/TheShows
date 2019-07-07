cc5d6dcdae6395396fd7a7b7cd976fa07039b45d
Jose L Crisostomo <jl.cs606@gmail.com>
2019-07-07 14:00:08 -0700

    Repository Layer ( Domain )
    added interfaces: Repository
    added implementations : RepositoryImpl::getShows()
    added testing : RepositoryImpl
    Added helper class : RxImmediateSchedulerRule

--- app/src/main/java/com/simplekjl/showsapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/simplekjl/showsapp/data/RepositoryImpl.kt
@@ -8,7 +8,7 @@ import io.reactivex.Observable
  *  Repository decides where to take data, it can be Database, Cache or Network
  */
 
-class RepositoryImpl constructor(val network: Network) : Repository {
+class RepositoryImpl constructor(private val network: Network) : Repository {
 
     override fun getShows(page: Int): Observable<ResponseState> {
         return network.getShows(page).toObservable()
---
+++ app/src/test/java/com/simplekjl/showsapp/data/RepositoryImplTest.kt
@@ -0,0 +1,68 @@
+package com.simplekjl.showsapp.data
+
+import com.nhaarman.mockitokotlin2.whenever
+import com.simplekjl.showsapp.data.model.ErrorResponse
+import com.simplekjl.showsapp.data.model.NotFoundResponse
+import com.simplekjl.showsapp.data.model.Show
+import com.simplekjl.showsapp.data.model.ShowsResponse
+import com.simplekjl.showsapp.data.remote.Network
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class RepositoryImplTest {
+
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    @Mock
+    lateinit var mockNetwork: Network
+
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getShows() {
+        val showList = listOf(
+            Show(
+                "", 0, 0, "", 0,
+                "", "", ""
+            )
+        )
+        val showsResponse = ShowsResponse(0, showList, 12, 0)
+        whenever(mockNetwork.getShows(1)).thenReturn(Single.create { it.onSuccess(showsResponse) })
+        mockNetwork.getShows(1)
+            .test()
+            .assertNoErrors()
+            .onNext(showsResponse)
+    }
+
+    @Test
+    fun getShowsNotFoundResponse() {
+        val notFoundResponse = NotFoundResponse("", 404)
+
+        whenever(mockNetwork.getShows(1)).thenReturn(Single.create { it.onSuccess(notFoundResponse) })
+        mockNetwork.getShows(1)
+            .test()
+            .assertNoErrors()
+            .onNext(notFoundResponse)
+    }
+
+    @Test
+    fun getShowsErrorResponse() {
+        val errorResponse = ErrorResponse("Api required", false, 401)
+
+        whenever(mockNetwork.getShows(1)).thenReturn(Single.create { it.onSuccess(errorResponse) })
+        mockNetwork.getShows(1)
+            .test()
+            .assertNoErrors()
+            .onNext(errorResponse)
+    }
+}
